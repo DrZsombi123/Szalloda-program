5876121797ac79a2dfe1f65ce7094f56aaebf57f
firelight-dev <118626431+DrZsombi123@users.noreply.github.com>
2026-04-14 17:01:18 +0200

    Projekt létrehozás + Foglalas osztály

---
+++ Foglalas.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace szallodaprogram
+{
+    class Foglalas
+    {
+        public int Id { get; set; }
+        public string VendegNev { get; set; }
+        public int SzobaSzam { get; set; }
+        public DateTime Erkezes { get; set; }
+        public DateTime Tavozas { get; set; }
+        public int ArPerEjszaka { get; set; }
+
+        public int Ejszakak => (Tavozas - Erkezes).Days;
+        public int TeljesAr => Ejszakak * ArPerEjszaka;
+    }
+}
